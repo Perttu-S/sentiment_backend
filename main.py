fadf7c88c93667bf89019c603776d2a8680b6d42
Perttu-S <psimonta@edu.lapinamk.fi>
2024-05-02 19:11:27 +0300

    asd

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, request
+import pickle
+
+# Load the model
+model = pickle.load(open('sentiment_analysis_model.pkl', 'rb'))
+
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def sentiment_analysis():
+    data = request.get_json()
+    text = data['text']
+
+    #sentiment analysis
+    prediction = model.predict([text])
+    response = {
+        'text': text,
+        'sentiment': prediction[0]
+    }
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
